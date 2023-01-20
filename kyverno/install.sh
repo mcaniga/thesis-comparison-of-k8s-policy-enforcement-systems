7e7a5f95c96f192cb47e471f2638a6d919eff11f
Michal Čaniga <michalcaniga559@gmail.com>
2023-01-20 11:52:20 +0100

    Display relevant Kyverno installation info

--- kyverno/install.sh
+++ kyverno/install.sh
@@ -3,4 +3,6 @@
 # TODO: use helm? 
 # TODO: version can be specified also without helm, can't 'latest' in final form of thesis - https://kyverno.io/docs/installation/#install-kyverno-using-yamls
 
+echo "Installing latest Kyverno in Standalone mode"
+echo "For production installation, use 'helm' package manager for Kubernetes, specify exact version, and set at least 3 replicas." 
 kubectl create -f https://raw.githubusercontent.com/kyverno/kyverno/main/config/install.yaml
