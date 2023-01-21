0f315cafc3ba1f1800b8101f349d7bd32ebc196f
Michal Čaniga <michal.caniga@instea.sk>
2023-01-21 21:50:55 +0100

    Use fixed version of Kyverno

--- kyverno/install.sh
+++ kyverno/install.sh
@@ -2,15 +2,14 @@
 
 source k8s-helpers.sh
 
-# Install 'latest' Kyverno in 'Standalone' mode - only one replica, requests will be rejected when Kyverno is unavailable
+# Install Kyverno 1.7 in 'Standalone' mode - only one replica, requests will be rejected when Kyverno is unavailable
 # For production installation, use 'helm' package manager for Kubernetes, specify exact version, and set at least 3 replicas.
 # TODO: use helm?
-# TODO: version can be specified also without helm, can't 'latest' in final form of thesis - https://kyverno.io/docs/installation/#install-kyverno-using-yamls
 
 function install_kyverno {
   echo "Installing latest Kyverno in Standalone mode"
   echo "For production installation, use 'helm' package manager for Kubernetes, specify exact version, and set at least 3 replicas."
-  kubectl create -f https://raw.githubusercontent.com/kyverno/kyverno/main/config/install.yaml
+   kubectl create -f https://raw.githubusercontent.com/kyverno/kyverno/release-1.7/config/release/install.yaml
 }
 
 if namespace_exists "kyverno"; then
