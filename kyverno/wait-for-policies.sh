301832bf041f037778fcaabaaaf295c84d4ed096
Michal Čaniga <michal.caniga@instea.sk>
2023-01-22 13:06:34 +0100

    Use exit values in are_clusterpolicies_ready

--- kyverno/wait-for-policies.sh
+++ kyverno/wait-for-policies.sh
@@ -3,13 +3,23 @@
 source k8s-helpers.sh
 
 # Checks if cluster policies are ready
-# Returns "false" if cluster policies are not ready
+# Extracts 4th (READY) column, removes header line, check if at least one cluster policy has ready = "false"
+# Exits with 0 if cluster policies are ready, otherwise with 1
 function are_clusterpolicies_ready {
   # extracts 4th (READY) column, removes header line, check if at least one cluster policy has ready = "false"
-  kubectl get clusterpolicy -A | awk '{print $4}' | tail -n +2 | while read line; do [[ "$line" == "false" ]] && echo "false"; done
+  READINESS_FLAGS=$(kubectl get clusterpolicy -A | awk '{print $4}' | tail -n +2)
+  while read -r line
+  do
+    if [ $line == "false" ]; then
+      return 1;
+    fi
+  done < <($READINESS_FLAGS)
+  return 0;
 }
 
 # Check every second if Kyverno clusterpolicies are ready
-while [ $(are_clusterpolicies_ready) = "false" ]; do
+are_clusterpolicies_ready
+while [ $? -eq 1 ]; do
         sleep 1
+        are_clusterpolicies_ready
 done
