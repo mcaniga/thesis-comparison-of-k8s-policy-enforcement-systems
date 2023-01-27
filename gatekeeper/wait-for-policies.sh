a05ade535bce3158668628ad5693b82ea8434e24
Michal Čaniga <michal.caniga@instea.sk>
2023-01-27 13:44:43 +0100

    Update gatekeeper waiting for policy

--- gatekeeper/wait-for-policies.sh
+++ gatekeeper/wait-for-policies.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-# TODO
+# Applies gatekeeper constraints.
+# Uses global variable:
+#   $PROJECT_ROOT - path to the project root
+function apply_constraints {
+    # Wait for constraint templates to be applied
+    sleep 2
+    for constraint in "$PROJECT_ROOT"/gatekeeper/constraints/*; do
+      # TODO: maybe namespace should be added
+      kubectl apply -f $constraint >> $PROJECT_ROOT/exec.log
+    done
+    # Wait for constraints to be applied
+    sleep 2
+}
+
+apply_constraints
