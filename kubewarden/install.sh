1e8041fcaae235851f9808edf0764d1227f02ad8
Michal Čaniga <michal.caniga@instea.sk>
2023-01-27 10:53:36 +0100

    Add kubewarden installation

--- kubewarden/install.sh
+++ kubewarden/install.sh
@@ -2,17 +2,24 @@
 
 source k8s-helpers.sh
 
-# Install Kubewarden TODO: version
+# Install Kubewarden - crds 1.2.3, controller 1.4.0, defaults 1.5.1
+# Available chart versions: https://github.com/kubewarden/helm-charts/releases
 
 function install_kubewarden {
-  # TODO install
-  echo "Installing kubewarden..."
-  # TODO wait until installed
+  echo "Installing kubewarden (crds - 1.2.3, controller 1.4.0, defaults 1.5.1)..."
+  # Install cert manager - prerequisite of kubewarden
+  # TODO: specify exact version instead of latest
+  kubectl apply -f https://github.com/jetstack/cert-manager/releases/latest/download/cert-manager.yaml
+  kubectl wait --for=condition=Available deployment --timeout=2m -n cert-manager --all
+  # Install kubewarden
+  helm repo add kubewarden https://charts.kubewarden.io
+  helm install --wait -n kubewarden --create-namespace kubewarden-crds kubewarden/kubewarden-crds --version 1.2.3
+  helm install --wait -n kubewarden kubewarden-controller kubewarden/kubewarden-controller --version 1.4.0
+  helm install --wait -n kubewarden kubewarden-defaults kubewarden/kubewarden-defaults --version 1.5.1
 }
 
-# TODO
-#if <is gatekeeper installed>; then
-#  echo "Gatekeeper already installed";
-#else
-#  install_gatekeeper
-#fi
+if namespace_exists "kubewarden"; then
+  echo "Kubewarden already installed";
+else
+  install_kubewarden
+fi
