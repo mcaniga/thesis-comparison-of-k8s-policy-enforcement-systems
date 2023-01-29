6ddd80d3240f8633e32278624400248b796b795f
Michal Čaniga <michal.caniga@instea.sk>
2023-01-29 13:47:34 +0100

    Add kubewarden read only filesystem policy

--- kubewarden/wait-for-policies.sh
+++ kubewarden/wait-for-policies.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-# TODO
+# Waits until all kubewarden clusteradmissionpolicies in namespace are in ready state.
+# Accepts positional arguments:
+#   $1 - namespace
+# Uses global variable:
+#   $SC_PROJECT_ROOT - path to the project root
+function wait_until_clusteradmissionpolicies_are_ready {
+  kubectl wait --for=condition=ready clusteradmissionpolicy.policies.kubewarden.io -n $1 --all >>  $SC_PROJECT_ROOT/exec.log
+}
+
+wait_until_clusteradmissionpolicies_are_ready "kubewarden"
