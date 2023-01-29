e617ce371ca28f1ac9c87a24dd37df89e286e070
Michal Čaniga <michal.caniga@instea.sk>
2023-01-29 14:57:39 +0100

    Increase timeout for waiting for cluster admission policy to be active

--- kubewarden/wait-for-policies.sh
+++ kubewarden/wait-for-policies.sh
@@ -6,7 +6,9 @@
 # Uses global variable:
 #   $SC_PROJECT_ROOT - path to the project root
 function wait_until_clusteradmissionpolicies_are_ready {
-  kubectl wait --for=condition=PolicyActive clusteradmissionpolicy.policies.kubewarden.io -n $1 --all >>  $SC_PROJECT_ROOT/exec.log
+  sleep 10
+  # cluster admission policy takes long time to be ready, wait up to 2 minutes
+  kubectl wait --for=condition=PolicyActive clusteradmissionpolicy.policies.kubewarden.io -n $1 --all --timeout=120s >>  $SC_PROJECT_ROOT/exec.log
 }
 
 wait_until_clusteradmissionpolicies_are_ready "kubewarden"
