c51d5c7cb4f69d57df6cb014d77c08756dbfbe03
Michal Čaniga <michal.caniga@instea.sk>
2023-04-22 15:07:31 +0200

    Rename wasm

--- kubewarden/build-wasm.sh
+++ kubewarden/build-wasm.sh
@@ -8,8 +8,9 @@ for filename in *; do
     opa build -t wasm -e $POLICY_NAME/violation $POLICY_NAME.rego
     # Extract wasm policy from generated tar
     tar -xf bundle.tar.gz /policy.wasm
+    WASM_NAME=$(echo $POLICY_NAME | sed 's/\([a-z]\)\([A-Z]\)/\1-\2/g' | tr '[:upper:]' '[:lower:]')
     # Move wasm policy to wasm folder
-    mv ./policy.wasm ../wasm/$POLICY_NAME.wasm
+    mv ./policy.wasm ../wasm/$WASM_NAME.wasm
     # Cleanup
     rm bundle.tar.gz
 done
