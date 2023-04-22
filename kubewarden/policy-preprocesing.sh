cc74d8741efa8bd4caa795377a439a9bd3c238d9
Michal Čaniga <michal.caniga@instea.sk>
2023-04-22 15:17:29 +0200

    Preprocess kubewarden policies

--- kubewarden/policy-preprocesing.sh
+++ kubewarden/policy-preprocesing.sh
@@ -1,40 +1,21 @@
 #!/bin/bash
 
-# Converts casing - eg. 'hello-world' to helloWorld
-# Accepts positional arguments:
-#   $1 - input string
-#   $2 - path to wasm policy
-function convert_policy_casing_to_wasm_casing() {
-  local input_string=$1
-  local output_string=""
-
-  # Loop through each word in the input string
-  for word in $(echo $input_string | tr '-' ' '); do
-    # Convert the first character of each word to uppercase
-    output_string="${output_string}${word^}"
-  done
-
-  # convert first character to lower case
-  output_string=${output_string,}
-
-  echo "$output_string"
-}
-
-# Substitutes <<file>> placeholder in `spec.module` in kubewarden policies.
+# Substitutes <<ociRegistry>> placeholder in kubewarden policies.
 # Accepts positional arguments:
 #   $1 - path to yml policy
-#   $2 - path to wasm policy
 # Uses global variable:
 #   $SC_PROJECT_ROOT - path to the project root
-function substitute_path_placeholder_for_absolute_path {
+#   $SETTINGS_PATH - path from project root to parameters yaml
+function substitute_placeholders {
   local filename="$1"
-  local substitution="$2"
 
   # Read the contents of the file
   local file_contents="$(cat "$filename")"
 
-  # Replace the <<file>> placeholder with substitution
-  local updated_contents="${file_contents/<<file>>/$substitution}"
+  local oci_registry=$(yq eval '.ociRegistry' $SETTINGS_PATH)
+
+  # Replace placeholders  
+  local updated_contents="${file_contents/<<ociRegistry>>/$oci_registry}"
 
   # Overwrite the file with the updated contents
   echo "$updated_contents" > "$filename"
@@ -45,12 +26,7 @@ function substitute_path_placeholder_for_absolute_path {
 #   $SC_PROJECT_ROOT - path to the project root
 function preprocess_kubewarden_policies {
   for policy_path in "$SC_PROJECT_ROOT"/kubewarden/policies/*; do
-      local substitution="$SC_PROJECT_ROOT/kubewarden/wasm/"
-      local policy_filename=$(basename $policy_path)
-      local filename_without_extension="${policy_filename%.*}"
-      local wasm_filename=$(convert_policy_casing_to_wasm_casing $filename_without_extension)
-      local wasm_path="$SC_PROJECT_ROOT/kubewarden/wasm/$wasm_filename.wasm"
-      substitute_path_placeholder_for_absolute_path $policy_path $wasm_path
+      substitute_placeholders $policy_path
   done
   
 }
