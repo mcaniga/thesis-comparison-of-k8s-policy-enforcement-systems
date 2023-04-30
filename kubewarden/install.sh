b8707a333bb617f2d994755372586b9a87f10221
Michal Čaniga <michal.caniga@instea.sk>
2023-04-30 10:21:20 +0200

    Update kubewarden version

--- kubewarden/install.sh
+++ kubewarden/install.sh
@@ -2,11 +2,11 @@
 
 source k8s-helpers.sh
 
-# Install Kubewarden - crds 1.2.3, controller 1.4.0, defaults 1.5.1
+# Install Kubewarden - crds 1.3.0, controller 1.5.0, defaults 1.6.0
 # Available chart versions: https://github.com/kubewarden/helm-charts/releases
 
 function install_kubewarden {
-  echo "Installing kubewarden (crds - 1.2.3, controller 1.4.0, defaults 1.5.1)..."
+  echo "Installing kubewarden (crds - 1.3.0, controller 1.5.0, defaults 1.6.0)..."
   # Install cert manager - prerequisite of kubewarden
   kubectl apply -f https://github.com/jetstack/cert-manager/releases/latest/download/cert-manager.yaml >> $SC_PROJECT_ROOT/exec.log
   echo "Waiting for cert manager..."
@@ -14,11 +14,11 @@ function install_kubewarden {
   # Install kubewarden
   helm repo add kubewarden https://charts.kubewarden.io >> $SC_PROJECT_ROOT/exec.log
   echo "Waiting for crds..."
-  helm install --wait -n kubewarden --create-namespace kubewarden-crds kubewarden/kubewarden-crds --version 1.2.3 >> $SC_PROJECT_ROOT/exec.log
+  helm install --wait -n kubewarden --create-namespace kubewarden-crds kubewarden/kubewarden-crds --version 1.3.0 >> $SC_PROJECT_ROOT/exec.log
   echo "Waiting for controller..."
-  helm install --wait -n kubewarden kubewarden-controller kubewarden/kubewarden-controller --version 1.4.0 >> $SC_PROJECT_ROOT/exec.log
+  helm install --wait -n kubewarden kubewarden-controller kubewarden/kubewarden-controller --version 1.5.0 >> $SC_PROJECT_ROOT/exec.log
   echo "Waiting for defaults..."
-  helm install --wait -n kubewarden kubewarden-defaults kubewarden/kubewarden-defaults --version 1.5.1 >> $SC_PROJECT_ROOT/exec.log
+  helm install --wait -n kubewarden kubewarden-defaults kubewarden/kubewarden-defaults --version 1.6.0 >> $SC_PROJECT_ROOT/exec.log
 }
 
 if namespace_exists "kubewarden"; then
